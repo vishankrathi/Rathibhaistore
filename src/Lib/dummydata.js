dfa748c1177934d306105432f814b1b94b9fc9b8
Vishank <vishankrathi724@gmail.com>
2024-07-20 04:32:05 -0700

    first commit

---
+++ src/Lib/dummydata.js
@@ -0,0 +1,64 @@
+export const listdata=[
+    {
+    
+        id:1,
+        title:"A great Apartment Next to the beach",
+        img:"https://th.bing.com/th?id=OIP.UYMkFyOZGAkx9Xv9OzsBowHaJ4&w=216&h=288&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+        bedRooms:2,
+        bathRooms:1,
+        price:1000,
+        address:"456 park Avenue ,London",
+        latitude:51.5074,
+        longitude:-0.1278,
+    },
+    {
+        id:2,
+        title:"An Awesome Apartment  Near the park !Almost too",
+        img:"https://th.bing.com/th?id=OIP.nZpi1VHatIOCBZpY381UWgHaJ4&w=216&h=288&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+        bedRooms:3,
+        bathRooms:2,
+        price:1500,
+        address:"789 Oxford Street ,London",
+        latitude:52.5074,
+        longitude:-1.1278,
+    },
+    {
+        id:3,
+        title:"A New Apartment  in the city !",
+        img:"https://th.bing.com/th?id=OIP.n2cyonR5MVRPG5n73BG_MwHaF7&w=279&h=223&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+        bedRooms:3,
+        bathRooms:2,
+        price:1500,
+        address:"789 Oxford Street ,London",
+        latitude:52.5074,
+        longitude:-1.1278,
+    },
+    ];
+    export const SinglePostData={
+        id:1,
+        title:"Beatiful Apartement",
+        price:1200,
+        images:[
+            "https://wallpapercave.com/wp/wp6957327.jpg",
+            "https://cardiogenix.com/wp-content/uploads/2016/04/tropical-luxury-hotel-bedroom-with-tropical-luxury-hotel-bedroom-the-epitome-of-luxury.jpg",
+            "https://th.bing.com/th/id/OIP.FEyzBNIf133ELfjSznDbPwHaFj?w=800&h=600&rs=1&pid=ImgDetMain",
+            "https://i.pinimg.com/originals/1d/ca/00/1dca00cd5fc6fb8059c12ab134c7c6bf.jpg",
+        ],
+        bedrooms:2,
+        bathroom:1,
+        size:861,
+        latitude:51.5074,
+        longitude:-0.1278,
+        city:"london",
+        address:"1234 Breadway 5t",
+        school:"250m away",
+        bus:"100m away",
+        restaurant:"50m away",
+        description:"orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    };
+    export const userData={
+        id:1,
+        name:"john doe",
+        img:"https://images.pexels.com/photos/6102278/pexels-photo-6102278.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    }
+    
